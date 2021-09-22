9ead3c8564279c9067fcd2d816398bb09dddc8e9
jusju <jukka.juslin@haaga-helia.fi>
2021-09-22 10:08:55 +0300

    first!

--- src/viikko4/Kellonaika.java
+++ src/viikko4/Kellonaika.java
@@ -13,17 +13,34 @@ public class Kellonaika {
 	}
 	// METODIN MÄÄRITYS, YHDELLÄ PARAMETRILLÄ
 	public boolean tarkastaKellonaika(String kellonaika) {
-		return true;
+		// TUTKITAAN KELLONAJAN MUOTO
+		if(kellonaika.matches("\\d{2}:\\d{2}")) {
+			// SPLITATAAN KELLONAIKA : MERKIN KOHDALTA
+			String[] solut = kellonaika.split(":");
+			int tunnit = Integer.parseInt(solut[0]);
+			int minuutit = Integer.parseInt(solut[1]);
+			if(tunnit >= 0 && tunnit < 24 && minuutit >= 0 && minuutit < 60) {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
 	}
 	public static void main(String[] args) {
 		// METODIN KUTSUMISEKSI ON ENSIN LUOTAVA TÄSTÄ LUOKASTA OLIO 
 		Kellonaika olio = new Kellonaika();
 		// KUTSUTAAN METODIA KYSYKELLONAIKA
 		String aikasyote = olio.kysyKellonaika();
-		// MUISTA AINA KOKEILLA VAIHEITTAIN - PALASTELE
-		System.out.println(aikasyote);
+		// METODIN KUTSUMINEN PARAMETRILLÄ
+		if(olio.tarkastaKellonaika(aikasyote) == true) {
+			System.out.println("Kellonaika on oikein");
+		} else {
+			System.out.println("Kellonaika on väärin");
+		}
 		//Metodi kysyy ja palauttaa käyttäjän antaman kellonajan. Käyttäjän antama kellonaika luetaan merkkijono-muuttujaan.
-		
+		//Kellonaika on oikein
 		
 	}
 
